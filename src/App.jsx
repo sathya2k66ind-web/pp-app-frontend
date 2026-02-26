a18bddb7b663712784ee2149124a3ada39a82566
Sathya Narayanan <sathya2k66ind@gmail.com>
2026-02-26 10:27:04 +0530

    Connected Tailwind CSS to the project and added a postcss config file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <>
+      {/* Tailwind test line */}
+      <div className="p-8">
+        <h1 className="text-blue-500 text-4xl font-bold">Tailwind Works!</h1>
+      </div>
+
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<Login />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
